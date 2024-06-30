394c6c69c9f52dab0603df236ee08dc78cbaf1ce
mbrijmohan <mbrijmohan816@gmail.com>
2024-07-01 00:59:41 +0530

    Lecture 1

---
+++ Python_Practice_Apna_College/Lecture1.py
@@ -0,0 +1,94 @@
+print("Hello World")
+print("Paravind Kumar Vishwakarma")
+print("I have Completed MSc(IT)" , "with one year of experiance as IT Engineer")
+
+
+#Variables
+name = "Paravind Kumar Vishwakarma" #String
+age = 24 #Integer
+price = 30 #Float
+
+print("My name is : ", name)
+print("My age is : ", age)
+
+#Data Types
+"""
+Integers = +ve, -ve, 0
+String = "Paravind", 'Hi', '''World'''
+Float = Decimal Value
+Boolean = True and False (True and false always start with capital letter)
+None
+"""
+print(type(name)) #String
+print(type(age)) #Integer
+print(type(price)) #Float
+
+
+#Boolean Value
+age = 24
+old = True
+a = None
+print(type(old))
+print(type(a))
+
+
+#Keywords: Keywords are reserved words in Python
+"""
+and       else       in         return
+as        except     is         True
+assert    finally    lambda     try
+break     False      nonlocal   with
+class     for        None       while
+continue  from       not        yield
+def       global     or
+del       if         pass
+elif      import     raise
+"""
+
+#Print sum of two numbers
+a = 10
+b = 20 
+sum = a+b
+print("The sum of two number is : ",sum)
+
+# Types of Tokens
+# Punctuators : Punctuators are symbols to organize sentence in programming
+# (). {}. @. [], # etc
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
