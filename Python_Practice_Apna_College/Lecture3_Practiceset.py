f491be78d64ef5f69dcbdf9d3f9c1f16bbe9cb1a
mbrijmohan <mbrijmohan816@gmail.com>
2024-07-20 22:13:45 +0530

    Lecture 4 Practice Code

--- Python_Practice_Apna_College/Lecture3_Practiceset.py
+++ Python_Practice_Apna_College/Lecture3_Practiceset.py
@@ -8,6 +8,7 @@ movies.append(mov1)
 movies.append(mov2)
 movies.append(mov3)
 print(movies)
+print("\n")
 
 #or
 movieslist = []
@@ -18,9 +19,75 @@ movieslist.append(mov)
 mov = input("Enter the name of your third movies : ")
 movieslist.append(mov)
 print(movieslist)
+print("\n")
+
+#or
+moviesname = []
+moviesname.append(input("Enter the first movies"))
+moviesname.append(input("Enter the Second movies"))
+moviesname.append(input("Enter the Thirs movies"))
+print("\n")
 
 #Question 2
 print("WAP to check if a list contain a palindrome of elements")
+#Palindrove word = reverse word
+list1 = [1,2,1]
+list2 = [2,3,3]
+copy_list1 = list1.copy()
+copy_list1.reverse()
+
+if(copy_list1 == list1):
+    print("List is Palindrome")
+else:
+    print("List is not palindrome")
+print("\n")
+
+#Question 3
+print("""WAP to count the number of students with the "A" grade in the following tuple""")
+grade = ("A", "B", "C", "A", "A", "C", "D")
+print(grade.count("A"))
+print("\n")
+
+#Store the above values in a list & sort them "A" to "Z"
+gradelist = ["A", "B", "C", "A", "A", "C", "D"]
+print(gradelist.sort())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
---
+++ Python_Practice_Apna_College/Lecture4_Practice.py
@@ -0,0 +1,9 @@
+#Dictionary
+#Dictionary are used to store data values in key:value pairs
+#They are unordered, mutable(changeable) & don't allow dublicate keys
+info = {
+    "key" : "value",
+    "name":"Paravind Kumar Vishwakarma",
+    "Learning" : "Coding"
+} 
+print(info)
