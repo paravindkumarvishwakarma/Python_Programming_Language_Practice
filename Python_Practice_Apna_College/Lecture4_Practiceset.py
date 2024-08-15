a10ec710989d69837dcc5b5f17414b68c51be807
mbrijmohan <mbrijmohan816@gmail.com>
2024-08-15 22:58:50 +0530

    Loops

--- Python_Practice_Apna_College/Lecture4_Practiceset.py
+++ Python_Practice_Apna_College/Lecture4_Practiceset.py
@@ -17,7 +17,39 @@ print("\n")
 You are given a list of subjects for students, Assume one classroom is required for 1 subject. How many classrooms are needed by all students.
 "python","java","C++","python","javascript","java","python","java","C++","C"
 """
+set1 = {"python","java","C++","python","javascript","java","python","java","C++","C"}
+print(len(set1))
+print("\n")
 
+#Question 3
+"""
+WAP to enter marks of 3 subjects from the user and store them in a dictionary.
+Start with an empty dictionary & add one by one. Use Subject name as key & marks as value
+"""
+marks = {}
+x = int(input("Enter Physics marks : "))
+marks.update({"Physics" : x})
+y = int(input("Enter Maths marks : "))
+marks.update({"Maths" : y})
+z = int(input("Enter Chemistry marks : "))
+marks.update({"Chemistry" : z})
+a = int(input("Enter English marks : "))
+marks.update({"English" : a})
+b = int(input("Enter Hindi marks : "))
+marks.update({"Hindi" : b})
+print(marks)
+print("\n")
+
+#Question 4
+"""
+Figure out a way to store 9 & 9.0 as separate values in the set
+(You can take help of built-in data types)
+"""
+values = {
+    ("float", 9.0),
+    ("int", 9)
+}
+print(values)
 
 
 
---
+++ Python_Practice_Apna_College/Lecture5_Practice.py
@@ -0,0 +1,23 @@
+#Loops in python
+"""
+Loops are used to repeat instructions
+
+while loops 
+
+    while condtion : 
+        # some work
+"""
+
+#print hello 5 times
+count = 1
+while count <= 5:
+    print("Hello!")
+    count += 1
+
+#Iterator use for that variables which use as initialize the loops
+#Iteration the process of completing loops
+    
+
+
+
+
