c8a1c4f0872a3bacfc3908b0a50e68b50b76163a
mbrijmohan <mbrijmohan816@gmail.com>
2024-08-18 23:36:30 +0530

    Break and continue statement

--- Python_Practice_Apna_College/Lecture5_Practice.py
+++ Python_Practice_Apna_College/Lecture5_Practice.py
@@ -38,7 +38,23 @@ Break : Used to terminate the loop when encountered
 
 Continue : terminates execution in the current iteration & continues execution of the loop with the next iteration
 """
+i = 1
+while i <= 5:
+    print(i)
+    if(i == 3): 
+        break
+    i += 1
+print("End of the loop")
+print("\n")
 
+i = 0
+while i <= 5:
+    if(i == 3):
+        i += 1
+        continue #skip
+    print(i)
+    i += 1
+print("\n")
 
 
 
