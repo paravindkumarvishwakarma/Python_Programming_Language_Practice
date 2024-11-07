2183e0fbee8b0558c2499081fc1f07337d6172db
Paravind Kumar Vishwakarma <pravindkumarvishwakarma@gmail.com>
2024-11-07 14:34:35 +0000

    Convert Temperature

---
+++ Daily_Practice/D7_Convert_Temparature.py
@@ -0,0 +1,13 @@
+"""
+Convert Temperature
+Declare a variable "temperature" and initialize it with a value of 25.5 (in Celsius) and print it in Celsius and Kelvin (add 273 to the temperature in Celsius).
+
+[Note: Print the output in exactly the same format as given below. There is single space around hyphen(-)]
+
+Output Format
+Celsius - 25.5
+Kelvin - 298.5
+"""
+Temperature = 25.5
+print("Celsius - ", Temperature)
+print("Kelvin - ", Temperature + 273)
---
+++ Daily_Practice/D8_Area_and_Perimeter_of_Square.py
@@ -0,0 +1,8 @@
+"""
+Area and Perimeter of Square
+Write a program to find the area and perimeter of square whose side length is 4.5. On the first line, print square's area and on the second line, its perimeter.
+
+Note:
+Area of a Square = side × side
+and, Perimeter = 4 x side
+"""
