2f242e7adc2d18f5a231a383eef3a7140126719d
Paravind Kumar Vishwakarma <pravindkumarvishwakarma@gmail.com>
2024-11-07 13:35:15 +0000

    Divide Two Variable

---
+++ Daily_Practice/D6_Divide_Two_Variable.py
@@ -0,0 +1,12 @@
+"""
+Divide Two Variables
+  Declare two variables x and y, and assign 
+  20
+  20 to x and 
+  6
+  6 to y.
+  Divide x by y (i.e., perform integer division x // y) and print the result.
+"""
+x = 20
+y = 6
+print(x // y)
