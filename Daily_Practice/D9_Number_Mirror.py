0a378e5187f4a364a1b1dd7b46d22aa7a9b4d964
Paravind Kumar Vishwakarma <pravindkumarvishwakarma@gmail.com>
2024-11-09 11:46:19 +0000

    Number Mirror

---
+++ Daily_Practice/D9_Number_Mirror.py
@@ -0,0 +1,21 @@
+"""
+Number Mirror
+Write a program that takes a number 
+N
+N as the input, and prints it to the output.
+
+Input Format
+The only line of input contains a single integer.
+
+Output Format
+Output the answer in a single line.
+
+Constraints
+0≤N≤10**5
+
+Sample 1:
+Input = 123
+Output = 123
+"""
+number = int(input("Enter a number: "))
+print(number)
