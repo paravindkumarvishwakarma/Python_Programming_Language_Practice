058eaaa665854ba92129507bed058331771d09e2
Paravind Kumar Vishwakarma <pravindkumarvishwakarma@gmail.com>
2024-11-09 12:57:10 +0000

    Rearrange Number

---
+++ Daily_Practice/D10_Rearrange_Number.py
@@ -0,0 +1,9 @@
+"""
+Rearrange Code
+You want to input two integers, and output their sum.
+
+Rearrange the code given, so that it does the above task.
+"""
+a = int(input())
+b = int(input())  
+print(a + b)
