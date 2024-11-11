632587cd14ba8aa8f7e0d64598ddfc22a9c3e9c9
Paravind Kumar Vishwakarma <pravindkumarvishwakarma@gmail.com>
2024-11-11 12:03:17 +0000

    Positive and Negative

---
+++ Daily_Practice/D14_Positive_and_Negative.py
@@ -0,0 +1,11 @@
+"""
+Positive and Negative
+Write a program to check whether a number given as user input is positive, negative, or zero.
+"""
+num = int(input("Enter a number = "))
+if num > 0:
+    print("Positive")
+elif num < 0:
+    print("Negative")
+else:
+  print("Zero")
