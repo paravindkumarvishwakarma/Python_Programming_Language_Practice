ff36990f5745485b7fb84c2877ce173d9b82e177
paravindkumarvishwakarma <pravindkumarvishwakarma@gmail.com>
2024-12-11 19:44:38 +0530

    Print_Square_Of_Matrics

---
+++ Daily_Practice/D19_Print_Square_of_Number.py
@@ -0,0 +1,11 @@
+"""
+Print Squares
+Write a program that utilizes a while loop to print the squares of numbers from 1 to N.
+
+Check the sample input / output below further clarity
+"""
+n = int(input("Enter a number : -"))
+a = 1
+while a<=n:
+    print(a**2)
+    a=a+n
