7eb204f456454ba52d6c0c4826cc9dc2eafebe0d
paravindkumarvishwakarma <pravindkumarvishwakarma@gmail.com>
2025-08-18 21:40:26 +0530

    Variables

---
+++ Python_Practice_Code_With_Harry/V2_Variables.py
@@ -0,0 +1,25 @@
+#Variables in Python
+A = "Ram" #string variable
+print(type(A))
+
+B = 10 #integer variable
+print(type(B))
+
+C = 20.5 #float variable
+print(type(C))
+
+d= True #boolean variable
+print(type(d))
+
+fruits = ["Apple", 'Banana', 'Cherry'] #list variable #mutable
+print(type(fruits))
+
+Animals = ("Dog", "Cat", "Elephant") #tuple variable #immutable
+print(type(Animals))
+
+dict= {"name": "John", "age": 30, "city": "New York"} #dictionary variable
+print(type(dict))
+
+unique_id = {1,2,3,4,5,6,7,8,9} #set variable
+print(type(unique_id))
+
