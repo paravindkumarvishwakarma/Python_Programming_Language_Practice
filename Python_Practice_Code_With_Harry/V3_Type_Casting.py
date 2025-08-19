c49bbf5e6e23c589472423659d745e1cd8165274
paravindkumarvishwakarma <pravindkumarvishwakarma@gmail.com>
2025-08-19 17:55:41 +0530

    Type Casting Practice Added

---
+++ Python_Practice_Code_With_Harry/V3_Type_Casting.py
@@ -0,0 +1,27 @@
+#Type casting in Python
+
+"""
+Explicit Typecasting (Manual)
+You tell Python to convert the type using functions like:
+
+int() – converts to integer
+float() – converts to float
+str() – converts to string
+bool() – converts to boolean
+"""
+
+#Convert float to int:
+x = 9.8
+y = int(x)
+print("y =",y) #Output: y = 9
+
+#Convert string to int:
+x = "10"
+y = int(x)
+print(y+1) #Output: 11
+
+#Convert int to string
+n = 100
+y = str(n)
+print(y+" is a number") #Output: 100 is a number
+
