a4bc62da5ca4bd410cd4ec5c8f350180f144aeb8
paravindkumarvishwakarma <pravindkumarvishwakarma@gmail.com>
2025-08-25 23:28:29 +0530

    New Question solved on file User Input

---
+++ Python_Practice_Code_With_Harry/Test.py
@@ -0,0 +1,4 @@
+print("Que-7. Ask the user to enter a sentence and print the first and last character of the sentence.")
+print("\n")
+Sentance = input("Enter a sentance of your choice = ")
+print(Sentance[0]+Sentance[-1])
