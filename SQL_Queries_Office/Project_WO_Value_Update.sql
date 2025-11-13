26e951873a98054068395cad56d6795117650c27
paravindkumarvishwakarma <pravindkumarvishwakarma@gmail.com>
2025-11-13 11:29:40 +0530

    Work Order Value Updated Query

--- SQL_Queries_Office/Project_WO_Value_Update.sql
+++ SQL_Queries_Office/Project_WO_Value_Update.sql
@@ -1,7 +1,15 @@
---Project Work Order Value Update
+select wovalue,* from vtplhproject where projcode='EP-1265'
+select wovalue,* from vmunmbranch where brancode='EP-1265'
+select wovalue,* from vtplhworkorder where projrfcd=101000395763
 
-select wovalue,wrkval,revwrkval,* from vtplhproject where projcode='EP-1231'
-select wovalue,* from vmunmbranch where brancode='EP-1231'
-select wovalue,* from vtplhworkorder where projrfcd=101000384414
 
---UPDATE vtplhproject SET wovalue= where serialno=101000384414
+
+
+begin tran
+UPDATE vtplhproject SET wovalue='68439044.00' where serialno=101000395763
+UPDATE vmunmbranch SET wovalue='68439044.00' where serialno=101000395763
+select wovalue,* from vtplhproject where projcode='EP-1265'
+select wovalue,* from vmunmbranch where brancode='EP-1265'
+rollback
+
+--Video Ref: https://padamsin-my.sharepoint.com/:v:/g/personal/paravind_padams_in/EVDCFcX9X7RNq4BmzlSIissB8OEVOopmgCpN9Z2IY3pwgQ?e=jLpS1c
